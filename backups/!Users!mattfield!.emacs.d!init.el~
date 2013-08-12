14f0cfb6f8ecd017fb1c6e1bcbbce7d84997c6ea
Matt Field <m.field42@gmail.com>
2013-08-12 12:58:29 +0100

    adding more package definitions

---
+++ backups/!Users!mattfield!.emacs.d!init.el~
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/better-defaults")
+(require 'better-defaults)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'zenburn t)
+
--- init.el
+++ init.el
@@ -1,5 +1,42 @@
+(push "/opt/boxen/homebrew/bin" exec-path)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+(setq inhibit-startup-message nil)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(blink-cursor-mode t)
+(show-paren-mode t)
+(column-number-mode t)
+(set-fringe-style -1)
+(tooltip-mode -1)
+
 (add-to-list 'load-path "~/.emacs.d/better-defaults")
 (require 'better-defaults)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
 
+(require 'package)
+(setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))
+(package-initialize)
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(add-to-list 'load-path "~/.emacs.d/el-get")
+(require 'el-get)
+
+(require 'haml-mode)
+
+(setq el-get-sources
+      '((:name ruby-mode
+               :type elpa
+               :load "ruby-mode.el")
+        (:name inf-ruby :type elpa)
+        (:name css-mode :type elpa)))
+(el-get 'sync)
