09d483dbd9de8962de0b7af1aa7008e2031d7d61
Matt Field <m.field42@gmail.com>
2013-08-13 11:11:00 +0100

    adds coffee-mode and smex

--- init.el
+++ init.el
@@ -40,10 +40,22 @@
         (:name zenburn-theme :type melpa)
         (:name haml-mode :type elpa)
         (:name magit :type marmalade)
-        (:name sass-mode)
-        (:name undo-tree)))
+        (:name sass-mode :type elpa)
+        (:name undo-tree :type elpa)
+        (:name smex :type melpa)
+        (:name js2-mode :type elpa)
+        (:name coffee-mode :type elpa)))
 (el-get 'sync)
 (load-theme 'zenburn t)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; Old M-x binding
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(add-to-list 'auto-mode-alist '("\\.coffee\\'" . coffee-mode))
