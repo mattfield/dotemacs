e5df011ed1ab9df462550382189545a92e190e23
Matt Field <m.field42@gmail.com>
2013-08-15 10:33:04 +0100

    adding new line functions and magit status keybinding

--- init.el
+++ init.el
@@ -74,3 +74,27 @@
 
 ;; Set RET to newline AND indent
 (define-key global-map (kbd "RET") 'newline-and-indent)
+
+(defun smart-open-line ()
+  "Insert an empty line after the current line.
+Position the cursor at its beginning, according to the current mode"
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent))
+
+(global-set-key [(shift return)] 'smart-open-line)
+(global-set-key (kbd "M-o") 'smart-open-line)
+
+(defun smart-open-line-above ()
+  "Insert an empty line above the current line.
+Position the cursor and it's beginning, according to the current mode"
+  (interactive)
+  (move-beginning-of-line nil)
+  (newline-and-indent)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key [(control shift return)] 'smart-open-line-above)
+(global-set-key (kbd "M-O") 'smart-open-line-above)
+
+(global-set-key (kbd "M-S") 'magit-status)
